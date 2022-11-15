167685b61e9999ff93a004c02ad9e1291805e8da
Gabrielle <gabrielle01.spence@gmail.com>
2022-11-14 20:01:05 -0800

    add ability to wait for walkEast

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -23,14 +23,17 @@ function newNonPlayableCharacter(x, y) {
 
     setInterval(moveCharacter, 1)
 
-    function walkEast() {
+    async function walkEast(time) {
         direction = 'east'
         element.src = `./assets/red-character/east.gif`
+        await sleep(time)
+        stop()
     }
 
     function walkNorth() {
         direction = 'north'
         element.src = `./assets/red-character/north.gif`
+        
     }
 
     function walkWest() {
@@ -56,4 +59,10 @@ function newNonPlayableCharacter(x, y) {
         walkSouth: walkSouth,
         stop: stop
     }
+}
+
+function sleep(time){
+    return new Promise(resolve =>{
+        setTimeout(resolve, time)
+    })
 }
